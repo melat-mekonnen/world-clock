dc9b10c872ac8ec1ad0b5a429378a7701bdb240b
melat-mekonnen <127474013+melat-mekonnen@users.noreply.github.com>
2024-11-18 05:00:52 -0800

    adding  local time

--- javascript/index.js
+++ javascript/index.js
@@ -22,7 +22,10 @@ function display_interval() {
     }
 }
 function updateCity(event) {
-  let cityTimeZone = event.target.value;
+    let cityTimeZone = event.target.value;
+    if (cityTimeZone ===("current")) {
+        cityTimeZone = moment.tz.guess();
+    }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
